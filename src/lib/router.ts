d16f71337130a4a5093dcc8a35dd00c84799f96c
lyqht <estee.tsw@gmail.com>
2022-08-24 22:41:57 +0800

    add multer and endpoint for file upload

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,9 +1,35 @@
+import { parse } from 'csv-parse';
 import express from 'express';
+import fs from 'fs';
+import multer from 'multer';
+import os from 'os';
 
 const router = express.Router();
+const upload = multer({ dest: os.tmpdir() });
 
 router.get('/hello', async (_req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+    res.status(200).json({ message: 'Hello World!' });
+});
+
+router.post('/upload', upload.single('file'), (req, res) => {
+    const file = req.file;
+
+    if (file) {
+        const data = fs.readFileSync(file.path);
+
+        parse(data, (err, records) => {
+            if (err) {
+                console.error(err);
+                return res
+                    .status(400)
+                    .json({ success: false, message: 'An error occurred' });
+            }
+
+            return res.json({ data: records });
+        });
+    }
+
+    res.status(400).json('No CSV File given');
 });
 
 export default router;
