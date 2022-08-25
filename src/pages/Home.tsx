4ef3e306e5a32c8e59f5f203fc1d6b16bd3682b0
lyqht <estee.tsw@gmail.com>
2022-08-26 01:49:24 +0800

    add random emoji display

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Restaurant } from '../../api/types/Restaurant';
 import FileUpload from '../components/FileUpload/FileUpload';
 
+const emoji = ['🍔', '🌯', '🥗', '🍜', '🍙', '🍕', '😫', '😵'];
+const getRandomItem = (items: string[]) =>
+    items[Math.floor(Math.random() * items.length)];
+
 function Welcome(): JSX.Element {
     const [restaurants, setRestaurants] = useState<Restaurant[] | null>(null);
 
@@ -20,7 +24,41 @@ function Welcome(): JSX.Element {
     return (
         <div className="p-12 h-screen">
             <div className="flex flex-col h-full">
-                <h1 className="text-5xl font-bold italic">Nomnom where?</h1>
+                <div className="flex flex-row justify-between">
+                    <h1 className="text-5xl font-bold italic">
+                        Nomnom where? {getRandomItem(emoji)}
+                    </h1>
+                    <div id="search-container" className="form-control">
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                placeholder="Search…"
+                                className="input input-bordered"
+                            />
+                            <button
+                                type="button"
+                                className="btn btn-square"
+                                title="search"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="py-8 h-3/4">
                     {restaurants ? (
                         <>
