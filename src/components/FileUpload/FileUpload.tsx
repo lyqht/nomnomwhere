ad0f965b483593b3b62f74c7fbc1cd6054f2178e
lyqht <estee.tsw@gmail.com>
2022-08-26 01:48:21 +0800

    improve fileupload ux

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -1,12 +1,18 @@
 import React, { Fragment, useState } from 'react';
 import axios from 'axios';
 
+type File = {
+    fileName: string;
+    filePath: string;
+};
+
 const FileUpload = () => {
     const [file, setFile] = useState('');
     const [filename, setFilename] = useState('Choose File');
     const [uploadedFile, setUploadedFile] = useState({});
     const [message, setMessage] = useState('');
-    const [uploadPercentage, setUploadPercentage] = useState(0);
+    const [userIsUploading, setUserIsUploading] = useState(false);
+    const [uploadPercentage, setUploadPercentage] = useState<number>(0);
 
     const onChange = (e) => {
         setFile(e.target.files[0]);
@@ -17,6 +23,8 @@ const FileUpload = () => {
         e.preventDefault();
         const formData = new FormData();
         formData.append('file', file);
+
+        setUserIsUploading(true);
 
         try {
             const res = await axios.post('/api/upload', formData, {
@@ -32,10 +40,9 @@ const FileUpload = () => {
             });
 
             setTimeout(() => setUploadPercentage(0), 10000);
-
             const { fileName, filePath } = res.data;
             setUploadedFile({ fileName, filePath });
-            setMessage('File Uploaded');
+            setMessage(`File Uploaded, ${res.data.data.length} records added`);
         } catch (err) {
             if (err.response.status === 500) {
                 setMessage('There was a problem with the server');
@@ -48,16 +55,16 @@ const FileUpload = () => {
 
     return (
         <Fragment>
-            {message ? <div>{message}</div> : null}
-            <p>
+            <p className="py-4">
                 You can also upload your own restaurants too! Refer{' '}
                 <a
                     target={'_blank'}
+                    className={'link link-primary'}
                     href="https://github.com/lyqht/nomnomwhere/blob/main/api/sampleFile.csv"
                 >
                     here
                 </a>{' '}
-                for an example.
+                for an example CSV file.
             </p>
             <form onSubmit={onSubmit}>
                 <div className="custom-file mb-4">
@@ -66,32 +73,30 @@ const FileUpload = () => {
                         className="custom-file-input"
                         id="customFile"
                         onChange={onChange}
+                        placeholder={''}
                     />
-                    <label className="custom-file-label" htmlFor="customFile">
-                        {filename}
-                    </label>
                 </div>
 
-                <div id="progress-bar">{uploadPercentage} % done</div>
+                <div id="progress-bar">
+                    {userIsUploading && (
+                        <>
+                            <p>{uploadPercentage} % done</p>
+                            <progress
+                                className="progress w-56"
+                                value={uploadPercentage}
+                                max="100"
+                            ></progress>
+                        </>
+                    )}
+                </div>
 
+                {message ? <div>{message}!</div> : null}
                 <input
                     type="submit"
                     value="Upload"
                     className="btn btn-primary btn-block mt-4"
                 />
             </form>
-            {uploadedFile ? (
-                <div className="row mt-5">
-                    <div className="col-md-6 m-auto">
-                        <h3 className="text-center">{uploadedFile.fileName}</h3>
-                        <img
-                            style={{ width: '100%' }}
-                            src={uploadedFile.filePath}
-                            alt=""
-                        />
-                    </div>
-                </div>
-            ) : null}
         </Fragment>
     );
 };
